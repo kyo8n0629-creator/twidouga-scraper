2ea540ea1723f3d0d46cdc01e8f872b034b4f38f
kyo8n0629-creator <kyo8n0629@gmail.com>
2026-05-29 20:08:22 +0900

    Update twidouga-scraper.js

--- twidouga-scraper.js
+++ twidouga-scraper.js
@@ -12,6 +12,7 @@ const CONFIG = {
   targetUrls: [
     'https://www.twidouga.net/jp/realtime_t1.php',
     'https://www.twiero.net/history',
+    'https://twihub.net/posts?type=realtime',
   ],
   flaresolverr: process.env.FLARESOLVERR_URL || 'http://localhost:8191',
   xfeedUrl:     process.env.XFEED_URL                  || 'https://5xfeed.com',
